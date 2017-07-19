6c17806699f814514a5557833ee6269a96460d3b
Santhosh <sthiyagarajan@owler.com>
2017-07-19 06:22:24 +0530

    rabbitmq and send email service

--- src/main/java/com/santhosh2129/email/server/commons/service/impl/EmailSendServiceImpl.java
+++ src/main/java/com/santhosh2129/email/server/commons/service/impl/EmailSendServiceImpl.java
@@ -5,10 +5,12 @@ import com.santhosh2129.email.server.rest.entiy.SendEmailEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by santhosh on 7/19/17.
  */
+@Service
 public class EmailSendServiceImpl implements EmailSendService {
     @Autowired
     private JavaMailSender javaMailSender;
